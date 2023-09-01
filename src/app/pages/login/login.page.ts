883ae09914a58e1b16b5ae385728264b5767993b
strpalgato <sebabored@gmail.com>
2023-09-01 10:40:00 -0400

    Logica basica de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular'; // Importa el componente AlertController
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,34 @@ export class LoginPage implements OnInit {
   username: string = ''; // Variable para el usuario
   password: string = ''; // Variable para la contraseña
 
-  autenticarLogin() {
-    // Requiere agregar logica
-  }
+  constructor(public alertController: AlertController) { } // Inyecta el componente AlertController
 
   ngOnInit() {
+  }
+
+  async autenticarLogin() {
+    // Requiere agregar logica
+
+    if (this.username === 'estudiante' && this.password === 'password') {
+      const alert = await this.alertController.create({
+        header: 'Éxito',
+        message: 'Ingreso correcto.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    } else {
+      // Si las credenciales son incorrectas, muestra un mensaje de error
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Credenciales incorrectas. Intente nuevamente.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+
+    }
+
   }
 
 }
