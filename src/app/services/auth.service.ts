e3d1d798e58b32e84616fa097f0e71122c57ae79
strpalgato <sebabored@gmail.com>
2023-10-21 21:42:30 -0300

    Login funcional con datos de JSON

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Users } from '../pages/interfaces/interfaces';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
+
+  GetUserById(codigo: any):Observable<Users>{
+    return this.httpclient.get<Users>(`${environment.apiUrl}/usuarios/?username=${codigo}`);
+  }
+
+  IsLogged(){
+    return sessionStorage.getItem('username')!=null;
+  }
+  
+
 }
