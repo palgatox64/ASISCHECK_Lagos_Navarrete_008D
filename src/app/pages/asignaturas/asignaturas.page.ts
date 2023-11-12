1030950a84580159f3919f6e2bb626069c5a8b91
strpalgato <sebabored@gmail.com>
2023-11-11 22:10:34 -0300

    Get de asignaturas funcional y ciclo for

--- src/app/pages/asignaturas/asignaturas.page.ts
+++ src/app/pages/asignaturas/asignaturas.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
-
 import { AuthService } from 'src/app/services/auth.service';
+
+
+
 
 @Component({
   selector: 'app-asignaturas',
@@ -10,13 +12,36 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class AsignaturasPage implements OnInit {
 
- 
+  
+  asignaturas: any[] = [];
 
-  constructor( private menuController: MenuController, private authService: AuthService) { }
+  constructor( private menuController: MenuController, private authService: AuthService) {  }
 
   ngOnInit() {
-   
+    
+    
   }
+
+  ionViewWillEnter() {
+    this.mostrarAsignaturas();
+  }
+
+  mostrarAsignaturas() {
+    this.authService.getSubjects().subscribe(
+      {
+        next: asignaturas => {
+          console.log('Asignaturas del usuario:', asignaturas);
+          this.asignaturas = asignaturas; // Asigna las asignaturas a la propiedad del componente
+        },
+        error: err => {
+          console.error('Error al obtener asignaturas:', err);
+        }
+      }
+    );
+  }
+
+
+
   mostrarMenu(){
     this.menuController.open('first');
   }
